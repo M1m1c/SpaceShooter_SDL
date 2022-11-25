3f814fcb90300e8d592685a060889cbd11d861e3
M1m1c <M1m1c@users.noreply.github.com>
2022-11-25 12:08:18 +0100

    added window and surface refernces

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
 
+#include "SDL.h"
+
 Game::Game()
 {
 }
@@ -8,14 +10,17 @@ Game::~Game()
 {
 }
 
-void Game::Init(SDL_Window* window)
+void Game::Init(SDL_Window* window, SDL_Surface* surface)
 {
+	m_Window = window;
+	m_Surface = surface;
 }
 
 void Game::Run()
 {
 	while (m_IsRunning) 
 	{
-
+		SDL_FillRect(m_Surface, NULL, SDL_MapRGB(m_Surface->format, 20, 20, 30));
+		SDL_UpdateWindowSurface(m_Window);
 	}
 }
--- Game.h
+++ Game.h
@@ -6,10 +6,13 @@ public:
 	Game();
 	~Game();
 
-	void Init(struct SDL_Window* window);
+	void Init(struct SDL_Window* window, struct SDL_Surface* surface);
 
 	void Run();
 
 private:
 	bool m_IsRunning = true;
+
+	struct SDL_Window* m_Window;
+	struct SDL_Surface* m_Surface;
 };
