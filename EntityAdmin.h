40644b03461f932e2bfe568b8da50dcee86926ce
M1m1c <M1m1c@users.noreply.github.com>
2022-11-29 14:49:59 +0100

    Changed use of Entity to EntityID

--- EntityAdmin.h
+++ EntityAdmin.h
@@ -11,22 +11,22 @@ class EntityAdmin
 public:
 	EntityAdmin()
 	{
-		for (Entity entity = 0; entity < MAX_ENTITIES; ++entity)
+		for (EntityID entity = 0; entity < MAX_ENTITIES; ++entity)
 		{
 			m_AvailableEntities.push(entity);
 		}
 	}
 
-	Entity CreateEntity()
+	EntityID CreateEntity()
 	{
 		assert(m_LivingEntityCount < MAX_ENTITIES && "Too many entities in existence.");
-		Entity id = m_AvailableEntities.front();
+		EntityID id = m_AvailableEntities.front();
 		m_AvailableEntities.pop();
 		++m_LivingEntityCount;
 		return id;
 	}
 
-	void DestroyEntity(Entity entity)
+	void DestroyEntity(EntityID entity)
 	{
 		assert(entity < MAX_ENTITIES && "Entity out of range.");
 		m_Signatures[entity].reset();
@@ -34,20 +34,20 @@ public:
 		--m_LivingEntityCount;
 	}
 
-	void SetSignature(Entity entity, CompSignature signature)
+	void SetSignature(EntityID entity, CompSignature signature)
 	{
 		assert(entity < MAX_ENTITIES && "Entity out of range.");
 		m_Signatures[entity] = signature;
 	}
 
-	CompSignature GetSignature(Entity entity)
+	CompSignature GetSignature(EntityID entity)
 	{
 		assert(entity < MAX_ENTITIES && "Entity out of range.");
 		return m_Signatures[entity];
 	}
 
 private:
-	std::queue<Entity> m_AvailableEntities{};
+	std::queue<EntityID> m_AvailableEntities{};
 
 	std::array<CompSignature, MAX_ENTITIES> m_Signatures{};
 
