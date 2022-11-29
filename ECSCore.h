4d67a6da693ab40956a3510361e6adfe21d04e3b
M1m1c <M1m1c@users.noreply.github.com>
2022-11-29 11:39:41 +0100

    renamed entity to EntityId

--- ECSCore.h
+++ ECSCore.h
@@ -8,6 +8,6 @@ const CompType MAX_COMPONENTS = 32;
 
 using CompSignature = std::bitset<MAX_COMPONENTS>;
 
-using Entity = std::uint32_t;
+using EntityID = std::uint32_t;
 
-const Entity MAX_ENTITIES = 10000;
+const EntityID MAX_ENTITIES = 10000;
