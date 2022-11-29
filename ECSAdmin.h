e29549cff059d758972073eef195abe88260c53f
M1m1c <M1m1c@users.noreply.github.com>
2022-11-29 10:54:11 +0100

    Added ECS Admin class wiht funcitons

---
+++ ECSAdmin.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <cstdint>
+#include <queue>
+#include <array>
+#include <cassert>
+
+#include "Components.h"
+
+using Entity = std::uint32_t;
+
+const Entity MAX_ENTITIES = 10000;
+
+class ECSAdmin
+{
+public:
+	ECSAdmin()
+	{
+		for (Entity entity = 0; entity < MAX_ENTITIES; ++entity)
+		{
+			m_AvailableEntities.push(entity);
+		}
+	}
+
+	Entity CreateEntity()
+	{
+		assert(m_LivingEntityCount < MAX_ENTITIES && "Too many entities in existence.");
+		Entity id = m_AvailableEntities.front();
+		m_AvailableEntities.pop();
+		++m_LivingEntityCount;
+		return id;
+	}
+
+	void DestroyEntity(Entity entity)
+	{
+		assert(entity < MAX_ENTITIES && "Entity out of range.");
+		m_Signatures[entity].reset();
+		m_AvailableEntities.push(entity);
+		--m_LivingEntityCount;
+	}
+
+	void SetSignature(Entity entity, CompSignature signature)
+	{
+		assert(entity < MAX_ENTITIES && "Entity out of range.");
+		m_Signatures[entity] = signature;
+	}
+
+	CompSignature GetSignature(Entity entity)
+	{
+		assert(entity < MAX_ENTITIES && "Entity out of range.");
+		return m_Signatures[entity];
+	}
+
+private:
+	std::queue<Entity> m_AvailableEntities{};
+
+	std::array<CompSignature, MAX_ENTITIES> m_Signatures{};
+
+	uint32_t m_LivingEntityCount{};
+};
