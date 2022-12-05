88c7664c7f02c14933e5ea4e10d85afca0cdef0e
M1m1c <M1m1c@users.noreply.github.com>
2022-12-05 18:43:00 +0100

    Added tag comp

--- Components.h
+++ Components.h
@@ -8,11 +8,19 @@ using Vector2 = glm::vec2;
 
 enum Inputs
 {
-	Up =	0,
-	Down =	1,
-	Left =	2,
+	Up = 0,
+	Down = 1,
+	Left = 2,
 	Right = 3,
 	Shoot = 4,
+};
+
+enum ObjectTag
+{
+	None,
+	Player = 1 << 0,
+	Enemy = 1 << 1,
+	Bullet = 1 << 2
 };
 
 struct TransformComp
@@ -39,4 +47,9 @@ struct RigidBodyComp
 struct InputComp
 {
 	std::bitset<5> InputSignature = 0;
+};
+
+struct TagComp 
+{
+	ObjectTag Tag = ObjectTag::None;
 };
