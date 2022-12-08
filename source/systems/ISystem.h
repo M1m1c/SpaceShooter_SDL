d80f544fd6103597531b4d4d52468f968989afb9
M1m1c <M1m1c@users.noreply.github.com>
2022-12-08 16:06:07 +0100

    Defined ISystem

--- source/systems/ISystem.h
+++ source/systems/ISystem.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <memory>
 class ISystem
 {
 public:
-	virtual ~ISystem() =0;
+	template<typename T,typename... Args>
+	static std::unique_ptr<ISystem> Create(Args&&... args);
 
-
+	virtual void Update(float deltaTime) = 0;
 };
+
+template<typename T, typename ...Args>
+inline std::unique_ptr<ISystem> ISystem::Create(Args && ...args)
+{
+	return std::make_unique<T>(args...);
+}
