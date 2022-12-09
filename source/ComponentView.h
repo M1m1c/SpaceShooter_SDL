802443b3dfd686618e4258083a309402c5761e67
M1m1c <M1m1c@users.noreply.github.com>
2022-12-09 16:59:40 +0100

    updated component view to use shared ptr

--- source/ComponentView.h
+++ source/ComponentView.h
@@ -8,7 +8,7 @@
 #include "CompArray.h"
 
 template<typename... Types>
-using DataTable = std::vector<std::tuple<Types&...>>;
+using DataTable = std::vector<std::tuple<Types...>>;
 
 
 
@@ -21,40 +21,40 @@ public:
 		const std::unordered_set<EntityID>& aliveEntites,
 		const std::array<CompSignature, MAX_ENTITIES>& signatures,
 		CompSignature relevantSignature,
-		CompArray<Components>&... arrays) :
+		std::shared_ptr <CompArray<Components>>... arrays) :
 		m_AliveEntities(aliveEntites),
 		m_Signatures(signatures),
 		m_RelevantSignature(relevantSignature),
 		m_Arrays{ arrays... }
 	{}
 
-	DataTable<std::tuple<Components&...>> Get() {
-		//TODO iterate over all alive entities, 
-		//check the m_signatures if the current entity ID has m_relevant signature
-		// then get refernces/ptrs to components from arrays and push them to the return array.
-		DataTable<std::tuple<Components&...>> table;
-		for (EntityID id : m_AliveEntities)
-		{
-			if ((m_Signatures[id] & m_RelevantSignature) == m_RelevantSignature)
-			{
-				std::tuple<Components&...> tuple;
+	//std::vector<std::tuple<Components*...>> Get() {
+	//	//TODO iterate over all alive entities, 
+	//	//check the m_signatures if the current entity ID has m_relevant signature
+	//	// then get refernces/ptrs to components from arrays and push them to the return array.
+	//	std::vector<std::tuple<Components*...>> table;
+	//	for (EntityID id : m_AliveEntities)
+	//	{
+	//		if ((m_Signatures[id] & m_RelevantSignature) == m_RelevantSignature)
+	//		{
+	//			std::tuple<Components*...> tuple;
 
-				std::apply([&tuple, &id](auto&... arrays)
-					{
-						std::tie(std::get<Components&>(tuple)...) = std::make_tuple(arrays[id]...);
-					}, 
-					m_Arrays);
+	//			std::apply([&tuple, id](auto&... arrays)
+	//				{
+	//					std::tie(std::get<Components>(tuple)...) = std::make_tuple(arrays[id]...);
+	//				}, 
+	//				m_Arrays);
 
-				table.push_back(tuple);
-			}
-		}
-		return table;
+	//			table.push_back(tuple);
+	//		}
+	//	}
+	//	return table;
 
-	}
+	//}
 
 private:
 	// Store references to the arrays of components.
-	std::tuple<CompArray<Components>&...> m_Arrays;
+	std::tuple<std::shared_ptr<CompArray<Components>>...> m_Arrays;
 
 	CompSignature m_RelevantSignature;
 	const std::array<CompSignature, MAX_ENTITIES>& m_Signatures;
