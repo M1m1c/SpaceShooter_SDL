ead9f033a4b9e0858eb57e9769dddccffecc1abb
M1m1c <M1m1c@users.noreply.github.com>
2022-12-13 16:59:07 +0100

    added default constructor

--- source/SpawnOrder.h
+++ source/SpawnOrder.h
@@ -5,6 +5,7 @@ struct SpawnOrder
 {
 public:
 	SpawnOrder(Vector2 spawnPos, ObjectTag tag, std::bitset<5> sig) : SpawnPosition(spawnPos), Tag(tag), InputSignature(sig) {}
+	SpawnOrder() = default;
 
 	Vector2 SpawnPosition = Vector2(0.f, 0.f);
 	ObjectTag Tag = ObjectTag::None;
