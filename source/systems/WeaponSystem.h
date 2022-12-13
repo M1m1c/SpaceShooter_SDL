d56f95b2cfe08e8bb4b98cb972fb7cf3de6874a1
M1m1c <M1m1c@users.noreply.github.com>
2022-12-13 16:58:52 +0100

    changed WeaponSystem to use spawn orders

--- source/systems/WeaponSystem.h
+++ source/systems/WeaponSystem.h
@@ -1,27 +1,32 @@
 #pragma once
 #include "ISystem.h"
+#include "../ECSCore.h"
 #include "../ComponentView.h"
 #include <memory>
 #include <glm/glm.hpp>
-class ECSRegistry;
-class Game;
+#include <queue>
 struct TransformComp;
 struct InputComp;
 struct TagComp;
 struct WeaponComp;
+struct SpawnOrder;
 
 class WeaponSystem : public ISystem
 {
 public:
 
-	WeaponSystem(const std::shared_ptr<ECSRegistry>& registry,std::shared_ptr<ComponentView<TransformComp, InputComp, TagComp, WeaponComp>> componentView): m_Registry(registry), m_ComponentView(componentView) {}
+	WeaponSystem(
+		std::queue<SpawnOrder>& orderQueue,
+		std::shared_ptr<ComponentView<TransformComp, InputComp, TagComp, WeaponComp>> componentView)
+		: m_OrderQueue(orderQueue), m_ComponentView(componentView) {}
+
 	~WeaponSystem() = default;
 
 	virtual void Update(float deltaTime) override;
 
 private:
-	void SpawnBullet( glm::vec2 position, int direction);
-	const std::shared_ptr<ECSRegistry>& m_Registry;
+	void SpawnBullet(glm::vec2 position, int direction);
+	std::queue<SpawnOrder>& m_OrderQueue;
 	std::shared_ptr<ComponentView<TransformComp, InputComp, TagComp, WeaponComp>> m_ComponentView;
 };
 
