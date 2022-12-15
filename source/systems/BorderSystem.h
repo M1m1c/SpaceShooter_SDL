7be63bf9cda61ebca151745572e59ed67157793b
M1m1c <M1m1c@users.noreply.github.com>
2022-12-15 13:23:50 +0100

    Defined BorderSystem

--- source/systems/BorderSystem.h
+++ source/systems/BorderSystem.h
@@ -1,10 +1,24 @@
 #pragma once
-class BorderSystem
+
+#include "ISystem.h"
+#include "../SystemView.h"
+#include "../Components.h"
+#include <memory>
+
+class BorderSystem :public ISystem
 {
 public:
-	BorderSystem()=default;
+	BorderSystem(
+		int width,
+		int height,
+		std::shared_ptr<SystemView<TransformComp, TagComp, HealthComp >> view) 
+		:m_ScreenWidth(width), m_ScreenHeight(height), m_SystemView(view) {  }
+
 	~BorderSystem()=default;
 
-private:
+	virtual void Update(float deltaTime) override;
 
+private:
+	std::shared_ptr<SystemView<TransformComp, TagComp, HealthComp >> m_SystemView;
+	int m_ScreenWidth, m_ScreenHeight;
 };
