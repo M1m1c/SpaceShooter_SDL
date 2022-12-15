1bd1c228c731fea46390ddf69a1cef7bcab492e7
M1m1c <M1m1c@users.noreply.github.com>
2022-12-15 12:31:59 +0100

    rename compview to systemview

--- source/systems/ThrottleSystem.h
+++ source/systems/ThrottleSystem.h
@@ -8,10 +8,10 @@ struct InputComp;
 class ThrottleSystem: public ISystem
 {
 public:
-	ThrottleSystem(std::shared_ptr<SystemView<RigidBodyComp,InputComp>> componentView) : m_ComponentView(componentView){}
+	ThrottleSystem(std::shared_ptr<SystemView<RigidBodyComp,InputComp>> componentView) : m_SystemView(componentView){}
 	~ThrottleSystem() = default;
 
 	virtual void Update(float deltaTime) override;
 private:
-	std::shared_ptr<SystemView<RigidBodyComp, InputComp>> m_ComponentView;
+	std::shared_ptr<SystemView<RigidBodyComp, InputComp>> m_SystemView;
 };
